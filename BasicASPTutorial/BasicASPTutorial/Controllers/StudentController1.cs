f83ae51617ff638e4d95c93f6e04bbe0749302bf
balldev1 <balldev1@hotmail.com>
2023-05-10 16:22:00 +0700

    first commit

---
+++ BasicASPTutorial/BasicASPTutorial/Controllers/StudentController1.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BasicASPTutorial.Controllers
+{
+    public class StudentController1 : Controller
+    {
+        public IActionResult Index()
+        {
+            return Content("Idadndex");
+        }
+    }
+}
---
+++ BasicASPTutorial/BasicASPTutorial/Models/Student.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace BasicASPTutorial.Models
+{
+    public class Student
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Please enter student name")]
+        [DisplayName("Name Student")]
+        public string Name { get; set; }
+        [DisplayName("Score")]
+        [Range(0, 100,ErrorMessage ="Plese enter a score in the range 0-100")]
+        public int Score { get; set; }
+    }
+}
